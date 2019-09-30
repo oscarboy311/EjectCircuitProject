f9b973c0ceb11879d0a2c9c2f97713b7e9a036e9
oscarboy311 <oscarboy311@gmail.com>
2019-09-30 00:09:26 -0400

    Added logic related to the relay and the preset altitude

--- EjectionCircuit/EjectionCircuit.ino
+++ EjectionCircuit/EjectionCircuit.ino
@@ -1,9 +1,11 @@
 #include <Adafruit_BMP085.h>
 const int ledPin = 13; //assign an int value to the led pin number
 const int eFuse = 12; //assign an int value to the electric fuse 
+const float threshold = 100;
+const float presetAltitude = 0;
 
 float bias = 0;
-float threshold = 0;
+
 Adafruit_BMP085 bmp;
 
 void setup() {
@@ -28,16 +30,51 @@ void loop() {
   Serial.print(bias);
   Serial.println(" meters");
 
-  // Event Threshold
+  // Event Threshold (Apogee)
   float currReading = 0;
   do{
     // Read Sensor
-    currReading = bmp.readAltitude();
+    currReading = getAltitude();
   }
   while(currReading < threshold);
+  Serial.println("Threshold reached!");
 
-  // ...
-  float prevReading = bmp.readAltitude();  
+  // Verify descent
+  float tally = 0;
+  float prevReading = 0;
+  bool descending = false;
+  while(!descending){
+    prevReading = getAltitude();
+    delay(100);
+    currReading = getAltitude();
+    if(currReading < prevReading){
+      tally++;
+      if(tally > 30){
+        descending = true;
+      }
+    }
+  }
+  Serial.println("Rocket is descending!");
+
+  // Activate Relay + Turn on LED
+  digitalWrite(eFuse, HIGH);
+
+  // Verify if preset altitude has been reached
+  bool presetReached = false;
+  while(!presetReached){
+    currReading = getAltitude();
+    if(currReading < presetAltitude){
+      presetReached = true;
+    }  
+  }
+  Serial.println("Preset altitude reached!");
+  
+  // Turn off LED
+  digitalWrite(eFuse, LOW);
+
+  // END
+  while(true);
+  
 }
 
 float getBias(){
@@ -49,4 +86,8 @@ float getBias(){
     delay(10);//wait for 10 miliseconds
   }
   return (totalAltitudes/1000);
+}
+
+float getAltitude(){
+  return bmp.readAltitude() - bias;
 }
